78249581abb85b4b381672de71435fa9a4d52253
Eka Risky F (ekarisky) <ekarisky@gmail.com>
2019-08-08 17:38:12 +0700

    add alertmanager, prometheus, & grafana port mapping
    Co-authored-by: Puja Pramudya <puja.pramudya@go-jek.com>

--- attributes/pathfinder_discovery.rb
+++ attributes/pathfinder_discovery.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2019, BaritoLog.
 
-# Pathfinder Discovery  directory
+# Pathfinder Discovery directory
 default["pathfinder_discovery"]["dir"] = "#{node["prometheus"]["dir"]}/pathfinder_discovery"
 default["pathfinder_discovery"]["log_dir"] = "#{node["prometheus"]["log_dir"]}"
 default["pathfinder_discovery"]["pathfinder_url"] = ""
@@ -12,7 +12,7 @@ default["pathfinder_discovery"]["pathfinder_containers_path"] = "/api/v2/ext_app
 default["pathfinder_discovery"]["pathfinder_nodes_path"] = "/api/v1/ext_app/nodes"
 default["pathfinder_discovery"]["pathfinder_token"] = ""
 
-#Port
+# Port mapping for each service
 default["pathfinder_discovery"]["zookeeper_port"] = "9141"
 default["pathfinder_discovery"]["kafka_port"] = "9308"
 default["pathfinder_discovery"]["kibana_port"] = "5601"
@@ -23,6 +23,9 @@ default["pathfinder_discovery"]["elasticsearch_port"] = "9108"
 default["pathfinder_discovery"]["barito_app_port"] = "3000"
 default["pathfinder_discovery"]["postgres_port"] = "9187"
 default["pathfinder_discovery"]["redis_port"] = "9121"
+default["pathfinder_discovery"]["alertmanager_port"] = "9093"
+default["pathfinder_discovery"]["prometheus_port"] = "9090"
+default["pathfinder_discovery"]["grafana_port"] = "3000"
 
 default["pathfinder_discovery"]["pathfinder_cluster"] = []
 default["pathfinder_discovery"]["pathfinder_scrape_type"] = {
