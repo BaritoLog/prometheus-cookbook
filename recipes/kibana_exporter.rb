39d8f4840b15b9d216d3111622d1c0f3230aa6be
Eka Risky F (ekarisky) <ekarisky@gmail.com>
2019-08-13 16:34:35 +0700

    change kitchen custom spec & add condition for kibana exporter
    Co-authored-by: Puja Pramudya <puja.pramudya@go-jek.com>

--- recipes/kibana_exporter.rb
+++ recipes/kibana_exporter.rb
@@ -4,7 +4,18 @@
 #
 # Copyright:: 2019, BaritoLog.
 
+module Plugin
+  def self.exists?
+    cmd = Mixlib::ShellOut.new("bin/kibana-plugin list", cwd: node["kibana_exporter"]["kibana_base_dir"])
+    cmd.run_command
+    cmd.stdout.include? "kibana-prometheus-exporter"
+  end
+end
+
 execute 'plugin-install' do
   cwd node["kibana_exporter"]["kibana_base_dir"]
   command "bin/kibana-plugin install #{node["kibana_exporter"]["url"]}"
+  not_if {
+    Plugin.exists?
+  }
 end
