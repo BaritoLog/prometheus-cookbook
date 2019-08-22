1a8087cfd349edb3166e9828a65139e0b3c36bdf
bentol <bentol@users.noreply.github.com>
2019-08-22 15:30:45 +0700

    bump metadata version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'baritolog@go-jek.com'
 license 'Apache-2.0'
 description 'Installs/Configures prometheus'
 long_description 'Installs/Configures prometheus'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/BaritoLog/prometheus-cookbook/issues'
