5ce1063c3f77090bd792f1c025c640d26668005c
Daniel <nieltansah@gmail.com>
2019-10-23 11:53:49 +0700

    Append prometheus user to lxd group

--- test/integration/lxd_exporter/default_test.rb
+++ test/integration/lxd_exporter/default_test.rb
@@ -2,6 +2,10 @@ control "lxd_exporter install" do
   impact 1.0
   title "Tests Prometheus LXD Exporter Installation"
 
+  describe user("prometheus") do
+    its("groups") { should include "lxd" }
+  end
+
   describe file("/opt/prometheus/lxd_exporter/lxd_exporter") do
     its("mode") { should cmp "0755" }
   end
