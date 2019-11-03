a4c3cfcc4fb20918320af1412b6e46d962c10790
Daniel <nieltansah@gmail.com>
2019-11-03 08:59:40 +0700

    Fix error on LXD exporter by bumping more version

--- Berksfile
+++ Berksfile
@@ -4,3 +4,5 @@ source "https://supermarket.chef.io"
 metadata
 
 cookbook "kibana_wrapper_cookbook", git: "https://github.com/BaritoLog/kibana_wrapper_cookbook"
+cookbook 'pathfinder-agent', git: "https://github.com/pathfinder-cm/pathfinder-agent-cookbook"
+cookbook "pathfinder-node", git: "https://github.com/pathfinder-cm/pathfinder-node-cookbook"
--- attributes/lxd_exporter.rb
+++ attributes/lxd_exporter.rb
@@ -8,8 +8,8 @@ default["lxd_exporter"]["dir"] = "#{node["prometheus"]["dir"]}/lxd_exporter"
 default["lxd_exporter"]["log_dir"] = "#{node["prometheus"]["log_dir"]}"
 default["lxd_exporter"]["binary"] = "#{node["lxd_exporter"]["dir"]}/lxd_exporter"
 
-default["lxd_exporter"]["version"] = "0.2.0"
-default["lxd_exporter"]["checksum"] = "310677ca3a0aae566d4e83ec59c5858024e832b972e65fed6811fe5e7abf0e43"
+default["lxd_exporter"]["version"] = "0.2.1"
+default["lxd_exporter"]["checksum"] = "ebc5efb5167fc25a1c5342725e398a928ae346234175651854809e8797e4c12c"
 default["lxd_exporter"]["binary_url"] = "https://github.com/BaritoLog/lxd_exporter/releases/download/v#{node["lxd_exporter"]["version"]}/lxd_exporter-#{node["lxd_exporter"]["version"]}.linux-amd64.tar.xz"
 
 default["lxd_exporter"]["lxd_socket"] = "/var/snap/lxd/common/lxd/unix.socket"
