35f3a09c017d1ada5e8be5fa73375541551647fd
Eka Risky F (ekarisky) <ekarisky@gmail.com>
2020-01-28 18:09:57 +0700

    update cookbook version to 0.1.2
    Co-authored-by: Tara Baskara <tara.baskara@go-pay.co.id>

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# blackbox-exporter-wrapper CHANGELOG
+
+This file is used to list changes made in each version of the blackbox-exporter-wrapper cookbook.
+
+## 0.1.2
+- Add default blackbox exporter configuration
+- Add privileges to enable ICMP in blackbox exporter using `cap_net_raw`
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'baritolog@go-jek.com'
 license 'Apache-2.0'
 description 'Installs/Configures prometheus'
 long_description 'Installs/Configures prometheus'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/BaritoLog/prometheus-cookbook/issues'
