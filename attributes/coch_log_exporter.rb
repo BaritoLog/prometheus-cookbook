91f5aaf34dfe89455f4507e98a3439cee523db4b
Irwan Shofwan <abigaelse2@gmail.com>
2021-01-21 18:14:09 +0700

    Add arguments for coch-log-exporter

--- attributes/coch_log_exporter.rb
+++ attributes/coch_log_exporter.rb
@@ -1,5 +1,6 @@
-default["coch"]["dir"] = '/opt/conformance-checker-elasticalert-exporter'
-default["coch"]["binary"] = "#{node["coch"]["dir"]}/bin"
-default["coch"]["version"] = "0.1.0"
-default["coch"]["checksum"] = "dfb7529bc38658ed3c682ef3d55012c475acadf3f2793906bbf5dd9624188cac"
+default["coch"]["root_dir"] = '/opt/coch-log-exporter'
+default["coch"]["dir"] = '/opt/coch-log-exporter/bin'
+default["coch"]["binary"] = "#{node["coch"]["dir"]}/coch-log-exporter"
+default["coch"]["version"] = "0.1.1"
+default["coch"]["checksum"] = "3c3fe9a368a579ba9996018ab9ef164d86c8817833999e7b8b10ad0a5520d0e1"
 default["coch"]["binary_url"] = "https://github.com/ralibi/coch-log-exporter/releases/download/v#{node["coch"]["version"]}/coch-log-exporter_#{node["coch"]["version"]}_Linux_x86_64.tar.gz"
