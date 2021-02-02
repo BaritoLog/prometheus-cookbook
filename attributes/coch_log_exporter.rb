7154bdddea145b2020000135eedef223da133dc1
irwanshofwan <abigaelse2@gmail.com>
2021-02-02 17:05:43 +0700

    Upgrade coch-log-exporter to version 0.2.0

--- attributes/coch_log_exporter.rb
+++ attributes/coch_log_exporter.rb
@@ -1,6 +1,6 @@
 default["coch"]["root_dir"] = '/opt/coch-log-exporter'
 default["coch"]["dir"] = '/opt/coch-log-exporter/bin'
 default["coch"]["binary"] = "#{node["coch"]["dir"]}/coch-log-exporter"
-default["coch"]["version"] = "0.1.1"
-default["coch"]["checksum"] = "3c3fe9a368a579ba9996018ab9ef164d86c8817833999e7b8b10ad0a5520d0e1"
+default["coch"]["version"] = "0.2.0"
+default["coch"]["checksum"] = "ad40becb71a2c0117ca1f9a375c6c29e2ae5ac99428ce3870a20ccbbff160f07"
 default["coch"]["binary_url"] = "https://github.com/ralibi/coch-log-exporter/releases/download/v#{node["coch"]["version"]}/coch-log-exporter_#{node["coch"]["version"]}_Linux_x86_64.tar.gz"
